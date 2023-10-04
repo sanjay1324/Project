7d21bddef0d3505cb7adf1cf2db968ac33c9f4ae
sanjay1324 <sanjaymanickam07@gmail.com>
2023-10-04 16:07:09 +0530

    commit

--- my-react-app/src/pages/cart/cart.jsx
+++ my-react-app/src/pages/cart/cart.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import {ShopContext} from "../../context/shop-context"
+import {} from "../../products"
 export const Cart=() =>{
-
-    return <div> cart
+    const {cartItems} = useContext(ShopContext)
+    return <div className="cart"> 
+    <div>
+        <h1>
+            Your Cart Items
+        </h1>
+        <div className="cardItems"> 
+         {PRODUCTS.map((product)=>{
+            if(cartItems[product.id]!==0){
+              return < CartItem data={product} />  
+            }
+         })}
+        </div>
+    </div>
     </div>
 }
