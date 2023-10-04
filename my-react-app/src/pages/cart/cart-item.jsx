9850fa5b10136ddb2029123e1fbd3ebdd64be99b
sanjay1324 <sanjaymanickam07@gmail.com>
2023-10-04 17:54:37 +0530

    commit

--- my-react-app/src/pages/cart/cart-item.jsx
+++ my-react-app/src/pages/cart/cart-item.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {ShopContext} from "../../context/shop-context"
+ 
 
-export const CartItem =()=>{
+export const CartItem =(props)=>{
 
+    
     const {id,productName,price,productImage} = props.data;
+    const {cartItems,addToCart,removeFromCart} = useContext(ShopContext)
     return (
         <div  className='cartItem'> 
            <img src= {productImage}/>
@@ -13,6 +17,11 @@ export const CartItem =()=>{
             </b>
            </p>
            <p> ${price}</p>
+           <div className="countHandler">
+                <button onClick={()=>removeFromCart(id)}> - </button>
+                <input value={cartItems[id]}/>
+                <button onClick={()=>addToCart(id)}> + </button>
+           </div> 
            </div>
         </div>
     );
