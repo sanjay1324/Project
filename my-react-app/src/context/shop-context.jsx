d7826e95789651514738cfbfc2e6c3fd95fc3d3b
sanjay1324 <sanjaymanickam07@gmail.com>
2023-10-04 18:32:57 +0530

    commit

--- my-react-app/src/context/shop-context.jsx
+++ my-react-app/src/context/shop-context.jsx
@@ -11,6 +11,17 @@ const getDefaultCart = () =>{
 export const ShopContextProvider =(props) =>{
     const [cartItems,setCartItems] = useState(getDefaultCart());
 
+    const getTotalCartAmount =()=>{
+        let totalAmount = 0
+        for (const item in cardItems){
+            if(cartItems[item]>0){
+                let itemInfo= PRODUCTS.find((product)=> product.id===Number(item));
+                totalAmount += cartItems[item] * itemInfo.price
+            }
+        }
+        return totalAmount;
+    };
+
     const addToCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]: prev[itemId]+1}))
     }
@@ -18,9 +29,12 @@ export const ShopContextProvider =(props) =>{
     const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]: prev[itemId]-1}))
     }
-    console.log(cartItems)
 
-    const contextValue={cartItems,addToCart,removeFromCart};
+    const updateCartItemCount = (newAmount, itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+      };
+
+    const contextValue={cartItems,addToCart,removeFromCart,updateCartItemCount,getTotalCartAmount};
     return (
         <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
         
--- my-react-app/src/pages/cart/cart.jsx
+++ my-react-app/src/pages/cart/cart.jsx
@@ -4,7 +4,8 @@ import {PRODUCTS} from "../../products"
 import {CartItem} from "./cart-item"
 import "./cart.css"
 export const Cart = () =>{
-    const {cartItems} = useContext(ShopContext)
+    const {cartItems,getTotalCartAmount} = useContext(ShopContext)
+    const totalAmount = getTotalCartAmount();
     return <div className="cart"> 
     <div>
         <h1>
@@ -16,6 +17,14 @@ export const Cart = () =>{
               return < CartItem data={product} />  
             }
          })}
+        </div>
+
+        <div>
+            <p>
+                Subtotal: ${totalAmount}
+            </p>
+            <button> Continue Shopping</button>
+            <button> Checkout </button>
         </div>
     </div>
     </div>
